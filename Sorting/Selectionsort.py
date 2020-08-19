59f0e8f3fad7192b2204a15e6c950af30fc0a76c
ankurpareek <ankur.pareek20@gmail.com>
2020-08-19 22:35:45 +1000

    all python projects

---
+++ Sorting/Selectionsort.py
@@ -0,0 +1,20 @@
+def Selectionsort(inp):
+    for i in range(0,len(inp)-1):
+        minpos =i
+
+        for j in range(i,len(inp)):
+            minpos =j
+
+
+            if inp[j] > inp[j + 1]:
+                temp = inp[j]
+                inp[j] = inp[j + 1]
+                inp[j + 1] = temp
+
+
+inp = [1, 5, 2, 6, 9, 4]
+
+Selectionsort(inp)
+print(inp)
+
+
